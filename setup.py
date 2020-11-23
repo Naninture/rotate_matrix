9908f2f07339654995e2af43d8445e2e5746ba29
Naninture <49245785+Naninture@users.noreply.github.com>
2020-11-23 22:29:25 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,10 @@
 from setuptools import setup, find_packages
- 
+
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 classifiers = [
   'Development Status :: 5 - Production/Stable',
   'Intended Audience :: Education',
@@ -8,20 +13,17 @@ classifiers = [
   'Programming Language :: Python :: 3'
 ]
  
-from os import path
-this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
+
 
 
 setup(
   name='rotate_matrix',
-  version='0.0.5',
+  version='0.0.6',
   description='Rotate any matrix of any type, either clockwise or anti-clockwise instantly.',
   long_description=long_description,
   long_description_content_type='text/markdown',
   url='https://github.com/Naninture/rotate_matrix.git',  
-  download_url = 'https://github.com/Naninture/rotate_matrix/archive/0.0.5.tar.gz',
+  download_url = 'https://github.com/Naninture/rotate_matrix/archive/0.0.6.tar.gz',
   author='Udipta/Naninture',
   author_email='uddipta2255@gmail.com',
   license='MIT', 
